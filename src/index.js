58f19ab5f0f8a509e0a8b6cd718268401c2f8edb
Michael Paulukonis <mpauluko@travelers.com>
2018-10-06 11:00:20 -0400

    Converting to p5.js (non-working)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import p5 from 'p5';
+import P5 from 'p5'
 
+import TextManager from './sketch/TextManager'
+import Sketch from './sketch/sketch.js'
 
+// console.log(`APP-VERSION: ${VERSION}`)
 
-import sketch from './sketch';
+let t = new TextManager()
 
-new p5(sketch);
+function builder (p) {
+  myP5 = new Sketch(p, t)
+}
+
+var myP5 = new P5(builder)
 
